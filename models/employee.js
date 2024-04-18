93e8fe44fa44c452f0e8698e41d2540dc54132f9
Chouia Abderrahman <chouia.abderrahman.ad@gmail.com>
2024-04-18 13:00:36 +0100

    pg connection

--- models/employee.js
+++ models/employee.js
@@ -1,31 +1,42 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const dbURI = 'mongodb://mongo:27017/biometric-time-clock';
-//mongoose.connect('your-database-name', { useNewUrlParser: true, useUnifiedTopology: true });
+// const dbURI = 'mongodb://mongo:27017/biometric-time-clock';
+// //mongoose.connect('your-database-name', { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+// mongoose.connect(dbURI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// const db = mongoose.connection;
+
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', () => {
+//   console.log('Connected to MongoDB');
+// });
+
+// const employeeSchema = new mongoose.Schema({
+//   id: String,
+//   lastName: String,
+//   firstName: String,
+//   dateCreated: Date,
+//   department: String,
+//   checkIns: Date,
+//   checkOuts: Date,
+//   timeDifference: Number,
+//   comments: String,
+// });
+
+// module.exports = mongoose.model('Employee', employeeSchema);
+// console.log('ran');
+//////////////////////////////// postgres connection
+const { Client } = require('pg');
+
+const client = new Client({
+	user: 'node',
+	password: 'node',
+	host: 'localhost',
+	port: '5432',
+	database: 'node_db',
 });
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
-
-const employeeSchema = new mongoose.Schema({
-  id: String,
-  lastName: String,
-  firstName: String,
-  dateCreated: Date,
-  department: String,
-  checkIns: Date,
-  checkOuts: Date,
-  timeDifference: Number,
-  comments: String,
-});
-
-module.exports = mongoose.model('Employee', employeeSchema);
-console.log('ran');
+console.log('pg connection sucess');
